7415e90999d05b4738ac39e9b904863d541dd4f4
PetaUnix <cranny292@gmail.com>
2021-10-25 10:55:24 +0800

    My next commit

--- cart.php
+++ cart.php
@@ -29,7 +29,7 @@ if (isset($_POST['remove'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Cart</title>
+    <title>JOREX Cart</title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
